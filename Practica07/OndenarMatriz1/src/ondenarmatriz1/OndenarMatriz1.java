9f569fdf6e70f5adc2a91282eec8d906440c31b7
eaalvarado8 <eric_alvarado2202@hotmail.com>
2020-01-09 14:33:34 -0500

    Practica 07

--- Practica07/OndenarMatriz1/src/ondenarmatriz1/OndenarMatriz1.java
+++ Practica07/OndenarMatriz1/src/ondenarmatriz1/OndenarMatriz1.java
@@ -17,25 +17,70 @@ public class OndenarMatriz1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N;
-        
-        System.out.print("Ingrese tamaño vector: ");
-        N = sc.nextInt();
-        
-        int [] arreglo = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            System.out.print("Ingrese el valor " + (i + 1) + ": ");
-            arreglo[i] = sc.nextInt();
-            if (i >= 1) {
-                for (int j = 0; j < i; j++) {
-                    if (arreglo[i] == arreglo[j]) {
-                        System.out.print("Numero repetido, por favor ingrese" + " otro valor para " + (i + 1) + ": ");
-                        arreglo[i] = sc.nextInt();
-                    }
+        Scanner sc = new Scanner (System.in);
+        int n, m, p=0, z=0, N;
+        System.out.print("Ingrese numero de filas[fila,comlumna]: ");
+        n = sc.nextInt();
+        System.out.print("Ingrese numero de columnas[fila,comlumna]: ");
+        m = sc.nextInt();
+        int[][] matriz = new int[n][m];
+        for (int i = 0; i < n ; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print("Elemento [" + (i+1) + "," + (j+1) + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        ImprimirMatriz(matriz);
+        System.out.println();
+        N = n * m;
+        int[] vector = new int[N];
+        while( p < N){
+            for (int i = 0; i < n ; i++){
+                for(int j = 0; j < m; j++){
+                    vector[p] = matriz[i][j];
+                    p++;
                 }
             }
         }
+        vector = OrdenarVector(vector);
+        while( z < N){
+            for (int i = 0; i < n ; i++){
+                for(int j = 0; j < m; j++){
+                    matriz[i][j] = vector[z];
+                    z++;
+                }
+            }
+        }
+        ImprimirMatriz(matriz);
+        System.out.println();
+    }
+    public static void ImprimirMatriz(int x[][]){
+        for (int i = 0; i <= x.length - 1; i++) {
+            for (int j = 0; j <= x[0].length - 1; j++) {
+                System.out.print("+-----");
+            }
+            System.out.println("+");
+            for (int j = 0; j <= x[0].length - 1; j++) {
+                System.out.printf("| %3d ", x[i][j]);
+            }
+            System.out.println("|");
+        }
+        for (int j = 0; j <= x[0].length - 1; j++) {
+            System.out.print("+-----");
+        }
+        System.out.println("+");
+    }
+    public static int[] OrdenarVector(int[] x) {
+        int aux;
+        for (int i = 1; i <  x.length - 1 ; i++) {
+            for (int j = 0; j < x.length - i  ; j++) {
+                if (x[j] > x[j + 1]) {
+                    aux = x[j];
+                    x[j] = x[j + 1];
+                    x[j + 1] = aux;
+                }
+            }
+        }
+        return x;
     }
 }
--- Practica07/VectorSinRepetidos1/src/vectorsinrepetidos1/VectorSinRepetidos1.java
+++ Practica07/VectorSinRepetidos1/src/vectorsinrepetidos1/VectorSinRepetidos1.java
@@ -5,6 +5,8 @@
  */
 package vectorsinrepetidos1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
@@ -15,7 +17,25 @@ public class VectorSinRepetidos1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int N;
+        System.out.print("Ingrese tamaño vector: ");
+        N = sc.nextInt();
+        
+        int [] arreglo = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            System.out.print("Ingrese el valor " + (i + 1) + ": ");
+            arreglo[i] = sc.nextInt();
+            if (i >= 1) {
+                for (int j = 0; j < i; j++) {
+                    if (arreglo[i] == arreglo[j]) {
+                        System.out.print("Numero repetido, por favor ingrese" + " otro valor para " + (i + 1) + ": ");
+                        arreglo[i] = sc.nextInt();
+                    }
+                }
+            }
+        }
     }
-    
 }
+
